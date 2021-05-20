56835822e5cdd5720a7b2549e823fb1e2af965a4
Azat <kaumov.a.r@gmail.com>
2021-05-20 10:18:13 +0300

    Add buttons with constraints

--- AutoLayoutTutorial/ViewController.swift
+++ AutoLayoutTutorial/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if sender.title(for: .normal) == "X" {
+            sender.setTitle(
+                "A very long title for this button",
+                for: .normal
+            )
+        } else {
+            sender.setTitle("X", for: .normal)
+        }
+    }
+    
 }
 
